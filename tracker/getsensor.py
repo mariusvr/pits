e8546893b8280b2a05ef1813cfe536640a611d89
marius <marius@LAPTOP-GD8449J3>
2017-11-07 20:08:22 +1300

    updated sensor py

--- tracker/getsensor.py
+++ tracker/getsensor.py
@@ -13,14 +13,14 @@ print "IR light is : %.0f lux" %ir
 print "UV light is : %.0f lux" %uv
 print "Ozone concentration is : %.0f ppm" %ozone
 
-baseURL = "https://api.thingspeak.com/update?api_key=KA8898GKT8TYZUYQ&"
-baseURL += "field1=" + str(round(temperature,1))
-baseURL += "field2=" + str(round(humidity,0))
-baseURL += "field3=" + str(round(pressure,0))
-baseURL += "field4=" + str(round(ozone,0))
-baseURL += "field5=" + str(round(uv,0))
-baseURL += "field6=" + str(round(light,0))
-baseURL += "field7=" + str(round(ir,0))
+baseURL = "https://api.thingspeak.com/update?api_key=KA8898GKT8TYZUYQ"
+baseURL += "&field1=" + str(round(temperature,1))
+baseURL += "&field2=" + str(round(humidity,0))
+baseURL += "&field3=" + str(round(pressure,0))
+baseURL += "&field4=" + str(round(ozone,0))
+baseURL += "&field5=" + str(round(uv,0))
+baseURL += "&field6=" + str(round(visible,0))
+baseURL += "&field7=" + str(round(ir,0))
 urllib2.urlopen(baseURL).read()
 
 
